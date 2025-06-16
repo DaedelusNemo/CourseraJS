62893c70dbb0bef32c5d89c8313c7f3a2e815588
DaedelusNemo <daedelusnemo@yahoo.com>
2025-06-15 21:37:27 -0400

    finishing up

--- TodoList/TodoList.js
+++ TodoList/TodoList.js
@@ -34,5 +34,11 @@ function clearCompletedTasks() {
     displayTasks();
 }
 
+function clearAllTasks(){
+    tasks.length = 0;
+    displayTasks();
+}
+
 addTaskBtn.addEventListener("click", addTask);
 clearCompletedBtn.addEventListener("click", clearCompletedTasks);
+clearAllBtn.addEventListener("click", clearAllTasks);
