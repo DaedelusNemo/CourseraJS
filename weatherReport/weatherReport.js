748d4e273c35245120ffd3daa2c83cfe7db9d7d8
DaedelusNemo <daedelusnemo@yahoo.com>
2025-06-18 00:11:23 -0400

    message

--- weatherReport/weatherReport.js
+++ weatherReport/weatherReport.js
@@ -18,5 +18,26 @@ function showweatherDetails(event) {
         weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
     });
 }
+function showGridDetails(event) {
+    event.preventDefault();
+    const latitude = document.getElementById('latitude').value;
+    const longitude = document.getElementById('longitude').value;
+    const apiKey = 'c6ec3ca03bb243172c547c0ff8da25c5'; 
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
 
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const weatherGridInfo = document.getElementById('weatherGridInfo');
+            weatherGridInfo.innerHTML = `<h2>Weather at ${data.coord.lat}, ${data.coord.lon}</h2>
+                                    <p>Temperature: ${data.main.temp} &#8451;</p>
+                                    <p>Weather: ${data.weather[0].description}</p>`;
+    })
+    .catch(error => {
+        console.error('Error fetching weather:', error);
+        const weatherGridInfo = document.getElementById('weatherGridInfo');
+        weatherGridInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+    });
+}
 document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
+document.getElementById('gridForm').addEventListener('submit',showGridDetails );
